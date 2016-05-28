75304c2077841e7dfe921670eecc7e45247ba1d2
Kestas <komisarov@gmail.com>
2016-05-28 07:31:11 +0300

    Mygtuko pridėjimas pranesimo rodymui

--- src/main/java/lt/vilniuscoding/kalv/android/HelloAndroidActivity.java
+++ src/main/java/lt/vilniuscoding/kalv/android/HelloAndroidActivity.java
@@ -3,7 +3,11 @@ package lt.vilniuscoding.kalv.android;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HelloAndroidActivity extends Activity {
 
@@ -17,6 +21,18 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initActions();
+    }
+
+    private void initActions() {
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setText("Paspausk!");
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Paspaudete mygtuka", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 
     @Override
